91862692507145075d4b2090a7a8bf6de62cf288
George Perrett <georgeperrett@Georges-MacBook-Pro.local>
2021-01-11 13:39:53 -0500

    updated UI

--- UI/fit.R
+++ UI/fit.R
@@ -28,6 +28,14 @@ fit <- navbarMenu(title = "Fit Causal models",
                                  # CDescribe Data
                                  h4("Describe Data"),
                                  
+                                 # Grouping Variable
+                                 checkboxInput("gvarcheck", "Include Grouping Variable?", FALSE), 
+                                 
+                                 conditionalPanel(
+                                   condition = "input.gvarcheck",
+                                   selectInput("gvar", "Select Grouping Variable", choices = NULL)
+                                 ),
+                                 
                                  # Randomized
                                  radioButtons("rand", label = h6("Treatment Assignment"),
                                               choices = list("Non-Random" = 1, "Random" = 2), 
@@ -49,20 +57,12 @@ fit <- navbarMenu(title = "Fit Causal models",
                                  # Column Selection for Z, and identify treatment
                                  selectInput("zcol", "Select Treatment (Z) Column", choices = NULL),
                                  
-                                 #conditionalPanel(
                                  #  condition = "input.zcol",
                                  selectInput("trt.ind", "Type the Value Representing Receiving Treatment", 
                                              choices = NULL),
-                                 #)
                                  
-                                 # Grouping Variable
-                                 checkboxInput("gvarcheck", "Include Grouping Variable?", FALSE), 
-                                 
-                                 conditionalPanel(
-                                   condition = "input.gvarcheck",
-                                   selectInput("gvar", "Select Grouping Variable", choices = NULL)
-                                 )
-                                 
+                                 # select outcome (Y) variable
+                                 selectInput("ycol", "Select Outcome (Y) Column", choices = NULL)
                                ),
                                
                                # Main Panel
