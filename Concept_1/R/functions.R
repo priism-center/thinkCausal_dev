e873099f8c7b22d0fbbf8d172fcf36fc0a9dbd12
George Perrett <georgeperrett@Georges-MacBook-Pro.local>
2021-02-10 10:44:47 -0500

    added df function

--- Concept_1/R/functions.R
+++ Concept_1/R/functions.R
@@ -16,3 +16,24 @@ custom_datatable <- function(...){
                 )
   )
 }
+
+# example data set for demo 
+make_dat_demo <- function(n = 400){
+  inv.log <- function(x){exp(x*.8)/(1 + exp(x*.8))}
+  X <- runif(n, min = 0,max = 2)
+  X2 <- rnorm(n, -2, 5)
+  prob <- inv.log((-2 + (X^2)))
+  Z <- rbinom(n, 1, prob)
+  A <- matrix(runif(4), 2, 2)
+  A <- A %*% t(A)
+  diag(A) <- 1
+  Sigma <- A
+  X3 <- MASS::mvrnorm(n, rep(2, 2), Sigma)[, 1]
+  X4 <- MASS::mvrnorm(n, rep(2, 2), Sigma)[, 2]
+  Y0 <- 2*X + .8*X2 + rnorm(n, 0, 1)
+  Y1 <- 2*X + 3*(X^2) + .8*X2 +  rnorm(n, 0, 1)
+  df <- data.frame(X, prob, Z, Y0, Y1, X2, X3, X4)
+  df <- df %>% mutate(Y = if_else(Z ==1, Y1, Y0))
+  df <- df %>% dplyr::select(-c(Y0, Y1, prob))
+  return(df)
+}
