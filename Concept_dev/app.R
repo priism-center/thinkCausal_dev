a13934c11d1ec78f4b9bc9c8239ae518c18a02f0
TongJ <jintong5@gmail.com>
2021-04-15 12:54:37 -0400

    ver. 1.1
    Create data visualization for outputs

--- Concept_dev/app.R
+++ Concept_dev/app.R
@@ -4,7 +4,7 @@
 # Subtitle: Page - regression for causal
 # 
 # Data Created: 03/15/2021
-# Data Modified: 04/08/2021
+# Data Modified: 04/15/2021
 #
 # ----- ----- ----- ----- version
 # 
@@ -62,8 +62,8 @@ ui <- navbarPage(
              sidebarPanel(
                numericInput(inputId="tau", label = "Treatment Effect:", value = 5, min=0),
                hr(),
-               checkboxInput(inputId="var1", label = "Variable 1", value = TRUE),
-               checkboxInput(inputId="var2", label = "Variable 2", value = TRUE),
+               checkboxInput(inputId="var1", label = "Variable 1", value = FALSE),
+               checkboxInput(inputId="var2", label = "Variable 2", value = FALSE),
                checkboxInput(inputId="var3", label = "Variable 3", value = TRUE),
                checkboxInput(inputId="var4", label = "Variable 4", value = TRUE),
                checkboxInput(inputId="var5", label = "Variable 5", value = TRUE),
@@ -84,8 +84,11 @@ ui <- navbarPage(
                br(),
                plotOutput("plot0", width = "80%"),
                br(),
-               verbatimTextOutput("res0"),
-               br()
+               tableOutput("res0"),
+               br(),
+               verbatimTextOutput("res1"),
+               br(),
+               verbatimTextOutput("res2")
              ) # <END lr-simulator-sidebarLayout-mainPanel>
            ) # <END lr-simulator-sidebarLayout>
   ), # <END lr-simulator>
@@ -102,13 +105,21 @@ server <- function (input, output) {
   dgp <- eventReactive(
     eventExpr = input$generate, 
     valueExpr = {
+      X <- rnorm(n = input$sampsize, mean = 0, sd = 2)
       var1 <- rnorm(n = input$sampsize, mean = 5, sd = 2)
       var2 <- rnorm(n = input$sampsize, mean = 6, sd = 2)
       var3 <- rnorm(n = input$sampsize, mean = 7, sd = 2)
       var4 <- rnorm(n = input$sampsize, mean = 8, sd = 2)
       var5 <- rnorm(n = input$sampsize, mean = 9, sd = 2)
-      Y <- var1 + var2 + var3 + var4 + var5 + error(input$sampsize)
-      data0 <- data.frame(Y)
+      Z <- sample(c(0, 1), size = input$sampsize, replace = TRUE)
+      Y0 <- X + var1 + var2 + var3 + var4 + var5 +             error(input$sampsize)
+      Y1 <- X + var1 + var2 + var3 + var4 + var5 + input$tau + error(input$sampsize)
+      Y <- ifelse(
+        Z == 1,
+        yes = Y1,
+        no  = Y0
+      )
+      data0 <- data.frame(Y, X, Z)
       if (input$var1) {
         data0 <- cbind(data0, var1)
       }
@@ -127,27 +138,71 @@ server <- function (input, output) {
       return(data0)
     }
   )
+  dgp_full <- eventReactive(
+    eventExpr = input$generate, 
+    valueExpr = {
+      X <- rnorm(n = input$sampsize, mean = 0, sd = 2)
+      var1 <- rnorm(n = input$sampsize, mean = 5, sd = 2)
+      var2 <- rnorm(n = input$sampsize, mean = 6, sd = 2)
+      var3 <- rnorm(n = input$sampsize, mean = 7, sd = 2)
+      var4 <- rnorm(n = input$sampsize, mean = 8, sd = 2)
+      var5 <- rnorm(n = input$sampsize, mean = 9, sd = 2)
+      Z <- sample(c(0, 1), size = input$sampsize, replace = TRUE)
+      Y0 <- X + var1 + var2 + var3 + var4 + var5 +             error(input$sampsize)
+      Y1 <- X + var1 + var2 + var3 + var4 + var5 + input$tau + error(input$sampsize)
+      Y <- ifelse(
+        Z == 1,
+        yes = Y1,
+        no  = Y0
+      )
+      return(data0 = data.frame(
+        X, var1, var2, var3, var4, var5, Z, Y
+      ))
+    }
+  )
+  # mod_lm <- reactive({
+  #   data0 <- dgp()
+  #   return(
+  #     lm(Y ~ ., data = data0)
+  #   )
+  # })
+  # mod_lm_full <- reactive({
+  #   data0 <- dgp_full()
+  #   return(
+  #     lm(Y ~ ., data = data0)
+  #   )
+  # })
 
   observeEvent(input$generate, {
+    data0 <- dgp()
+    mod_lm <- lm(Y ~ ., data = data0)
+    data0 <- dgp_full()
+    mod_lm_full <- lm(Y ~ ., data = data0)
+    unbiased_Z <- summary(mod_lm_full)$coefficients['Z', 'Estimate']
+    sd_unbiased <- summary(mod_lm_full)$coefficients['Z', 'Std. Error']
+    estimated_Z <- summary(mod_lm)$coefficients['Z', 'Estimate']
+    sd_estimated <- summary(mod_lm)$coefficients['Z', 'Std. Error']
+    
+    data1 <- data.frame(
+      x = c("Truth", "Unbiased", "Problematic"),
+      y = c(input$tau, unbiased_Z, estimated_Z),
+      sd = c(0, sd_unbiased, sd_estimated)
+    )
 
-    # beta0 <- as.numeric(summary(mod_out)$coefficients[1])
-    # coefs <- as.numeric(summary(mod_out)$coefficients[2])
-    # r_sq <- summary(mod_out)$r.squared
-    # 
-    # output$plot0 <- renderPlot(
-    #   expr = {
-    #     data0 <- dgp()
-    #     ggplot(data0, aes(x = X, y = Y, color = factor(Z))) +
-    #       geom_point() +
-    #       xlab("Pretest Scores") +
-    #       ylab("Observed Outcome") +
-    #       labs(title = "Distribution of Observed Outcomes", color = "Treated?") +
-    #       scale_color_manual(values = c(blue, red)) +
-    #       theme_minimal(base_line_size = 0.4)
-    #   }
-    # )
-    output$res0 <- renderPrint(expr = {
-      lm(Y ~ ., data = dgp())
+    output$plot0 <- renderPlot(
+      ggplot(data1, aes(x = x, y = y)) + 
+        geom_point() + 
+        geom_errorbar(aes(ymin = y - sd, ymax = y + sd), width = .2,
+                      position = position_dodge(.9))
+    )
+    output$res0 <- renderTable(expr = {
+      xtable(data1)
+    })
+    output$res1 <- renderPrint(expr = {
+      summary(mod_lm)
+    })
+    output$res2 <- renderPrint(expr = {
+      summary(mod_lm_full)
     })
     
   }) # <END server-lr-simulator>
