e150e37dd70801926bd2e6fdf1124f542e0b5cec
George Perrett <georgeperrett@Georges-MBP.nyc.rr.com>
2021-05-21 13:03:30 -0400

    Create Production System

---
+++ Production/www/navSlideOver.js
@@ -0,0 +1,32 @@
+// open/closes the concepts sidebar
+function openConcepts() {
+  let sideDiv = document.getElementById("mySideBar")
+  if (sideDiv.style.width === 'min(100%, 700px)'){
+    sideDiv.style.width = '0';
+  } else {
+    sideDiv.style.width = "min(100%, 700px)"
+  }
+}
+
+// maybe replace with this https://css-tricks.com/sticky-smooth-active-nav/
+function openConceptsPage(divID) {
+  document.getElementById("mySideBar").style.width = "min(100%, 700px)";
+  let elmnt = document.getElementById(divID);
+  setTimeout(function(){elmnt.scrollIntoView(true);}, 750);
+  //let topPos = document.getElementById('Concept2').offsetTop
+  //setTimeout(function(){document.getElementById("conceptsSideBarContainer").scrollTop = topPos;}, 500);
+}
+
+function closeConcepts() {
+  document.getElementById("mySideBar").style.width = "0";
+}
+
+// wrap every h3 header in the concepts markdown in a div
+$(".markdownContainer h3").wrap("<div class='conceptsSubHeader'></div>");
+
+// add id to every h3 based on its title
+$('.conceptsSubHeader').each(function(){
+  id = $(this).text()
+  id = id.replace(/\s/g, '')
+  $(this).attr('id', id)
+})
