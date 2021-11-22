a931103c59e820915facc440ee5939bea12ff376
George Perrett <georgeperrett@Georges-MBP.lan>
2021-11-22 09:54:20 -0500

    patched blocking variables

---
+++ thinkCausal/R/fit_bart.R
@@ -0,0 +1,24 @@
+fit_bart <- function(.data, support, ran.eff, .estimand){
+  if(ran.eff == 'None'){
+      bartCause::bartc(
+        response = .data[, 1], 
+        treatment = .data[, 2], 
+        confounders = clean_confounders_for_bart(.data[, 3:length(.data)]), 
+        estimand = .estimand, 
+        commonSup.rule = support
+      )
+  }
+  
+  else{
+    bartCause::bartc(
+      response = .data[, 1], 
+      treatment = .data[, 2], 
+      confounders = clean_confounders_for_bart(.data[, 3:length(.data)]), 
+      estimand = .estimand, 
+      group.by = ran.eff, 
+      group.effects = T, 
+      commonSup.rule = support
+      
+    )
+  }
+}
