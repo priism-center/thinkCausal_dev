da8637d7b65d79896bf858cc016f6179b5f0b094
Joe Marlo <joe.marlo.09@gmail.com>
2022-01-31 08:52:55 -0500

    docs: added documentation for fit_bart(); added skeleton for progress bar

--- thinkCausal/R/fit_bart.R
+++ thinkCausal/R/fit_bart.R
@@ -1,28 +1,40 @@
+#' Fit a BART model using a standardized interface
+#'
+#' @param .data the data. First column should be treatment, second column response.
+#' @param support one of c('ate', 'atc', 'att'). See bartCause::bartc
+#' @param ran.eff if not "None", then the name of the column within .data that represents the random effects
+#' @param .estimand the causal estimand. See bartCause::bartc
+#'
+#' @return an object of class "bartcFit"
+#' @export
 fit_bart <- function(.data, support, ran.eff, .estimand){
-  if(ran.eff == 'None'){
+  tryCatch({
+    if(ran.eff == 'None'){
+        bartCause::bartc(
+          response = .data[, 2], 
+          treatment = .data[, 1], 
+          confounders = clean_confounders_for_bart(.data[, 3:length(.data)]), 
+          estimand = .estimand, 
+          commonSup.rule = support, 
+          keepTrees = TRUE, 
+          seed = 2
+        )
+    }
+    
+    else{
       bartCause::bartc(
-        response = .data[, 2], 
-        treatment = .data[, 1], 
+        response = .data[[2]], 
+        treatment = .data[[1]], 
         confounders = clean_confounders_for_bart(.data[, 3:length(.data)]), 
         estimand = .estimand, 
+        group.by = ran.eff, 
+        group.effects = TRUE, 
         commonSup.rule = support, 
-        keepTrees = T, 
+        keepTrees = TRUE,
         seed = 2
       )
-  }
-  
-  else{
-    bartCause::bartc(
-      response = .data[[2]], 
-      treatment = .data[[1]], 
-      confounders = clean_confounders_for_bart(.data[, 3:length(.data)]), 
-      estimand = .estimand, 
-      group.by = ran.eff, 
-      group.effects = T, 
-      commonSup.rule = support, 
-      keepTrees = T,
-      seed = 2
-      
-    )
-  }
+    }
+    },
+    error = function(e) NULL
+  )
 }
