083dcd1b9dd9ae8e2cb52366e9735770c6d0fb0f
joemarlo <joe.marlo.09@gmail.com>
2022-02-03 17:08:08 -0500

    implemented quiz wrapper as a module; drafted post-treatment learning module

---
+++ thinkCausal/R/module_quiz.R
@@ -0,0 +1,454 @@
+### this is the module that wraps a learning module with a quiz ###
+### see post-treatment learning module for an example ###
+
+require(shiny)
+require(shinyjs)
+
+ui_quiz <- function(id) {
+  ns <- NS(id)
+  tagList(
+    shinyjs::useShinyjs(),
+    uiOutput(outputId = ns('UI_quiz'))
+  )
+}
+
+server_quiz <- function(id, id_parent, question_texts, question_prompts, correct_answers, message_correct, message_wrong) {
+  ns <- NS(id)
+  moduleServer(
+    id,
+    function(input, output, session) {
+      # message(paste0('The quiz module has a namespace id of: ', id))
+      
+      # validate(need(length(questions) == length(answers),
+      #               'length(questions) must equal length(answers)'))
+      
+      # set the current state and potential values
+      store <- reactiveValues(
+        state = 'quiz-question-1',
+        states = c(paste0('quiz-question-', seq_along(question_texts)), 'quiz-complete'),
+        question_texts = question_texts,
+        question_prompts = question_prompts,
+        correct_answers = correct_answers,
+        responses = rep(NA, length(question_texts) + 1)
+      )
+      
+      # on button submit, manage the state
+      observeEvent(input$submit_button, {
+        
+        # record answers
+        store <- set_state(store, variable = 'current-response', value = input$answers)
+        
+        # is the answer correct
+        is_correct <- quiz_is_current_correct(store)
+        
+        # grade it
+        if (is_correct){
+          # add UI indicator
+          add_checkmark(ns = ns)
+          
+          # change the state
+          shinyjs::delay(1000, {
+            new_state <- get_state(store, variable = 'next-state')
+            store <- set_state(store, variable = 'current-state', value = new_state)
+          })
+        } else {
+          # add UI indicator
+          add_red_x(ns = ns)
+          
+          # change the state
+          shinyjs::delay(1000, {
+            store <- set_state(store, variable = 'current-state', value = 'quiz-complete')
+          })
+        }
+        
+      })
+      
+      # reset quiz
+      observeEvent(input$restart_button, {
+        # reset the state to the first question
+        store <- set_state(store, variable = 'current-state', value = 'quiz-question-1')
+        
+        # remove any responses
+        store$responses <- rep(NA, length(question_texts) + 1)
+        
+        # hide content
+        shinyjs::runjs('$(".learning-content").hide()')
+      })
+      
+      # render the UI
+      output$UI_quiz <- renderUI({
+        
+        if (get_state(store) == 'quiz-complete'){
+
+          # render ending message and confetti
+          all_correct <- quiz_is_all_correct(store)
+          if (all_correct) {
+            html_content <- tagList(br(),
+                                    add_message_correct(message_correct),
+                                    add_confetti())
+          } else {
+            html_content <- tagList(br(), add_message_wrong(message_wrong))
+          }
+
+          # make non-quiz content visible
+          shinyjs::runjs('$(".learning-content").show()')
+
+        } else {
+
+          # render the questions
+          html_content <- tagList(
+            p(get_state(store, 'current-question')),
+            get_state(store, 'current-answers'),
+            actionButton(inputId = NS(NS(id_parent)(id))('submit_button'),
+                         label = 'Submit',
+                         class = 'submit-button')
+          )
+
+          # wrap html in a div
+          html_content <- div(class = 'quiz-container',
+                              html_content)
+        }
+
+        # add the restart button
+        html_content <- tagList(
+          html_content,
+          br(),
+          actionButton(inputId = NS(NS(id_parent)(id))('restart_button'),
+                       label = 'Restart quiz',
+                       class = 'restart-button'),
+          br(), hr(), br(), br()
+        )
+
+        return(html_content)
+      })
+    }
+  )
+}
+
+
+
+# state machine -----------------------------------------------------------
+
+
+#' Manage the states of the quiz
+#'
+#' The quiz has states for each question and a final state for once the quiz ends. Only one state can be active at a time and the question text and answers shown depend on what state is active. 
+#' 
+#' These are `get` and `set` functions for retrieving state values and setting values. The states are originally created via a `reactiveValues` call within Shiny server (or `list` outside of Shiny; see example below).
+#'
+#' @param store a list formatted like in the example
+#' @param variable one of c('current-question', 'current-answers', 'current-correct-answer', 'next-state', 'current-response')
+#' @param state one of c('quiz-question-1', ..., 'quiz-question')
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' question_1 <- "This is question 1"
+#' question_2 <- "This is question 2"
+#' question_texts <- list(question_1, question_2)
+#' question_prompts <- list(c('1a', '1b'), c('2a', '2b'))
+#' correct_answers <- list(c('1a'), c('2b'))
+#' # use shiny::reactiveValues() in lieu of list()
+#' store <- list( 
+#'   state = 'quiz-question-1',
+#'   states = c(paste0('quiz-question-', seq_along(question_texts)), 'quiz-complete'),
+#'   question_texts = question_texts,
+#'   question_prompts = question_prompts,
+#'   correct_answers = correct_answers,
+#'   responses = c('yes', NA, NA)
+#' )
+#' get_state(store, 'current-question')
+#' @describeIn get_state a getter function for the state machine
+get_state <- function(store, variable = NULL, state = NULL){
+  if (is.null(state)) state <- store$state
+  if (is.null(variable)) return(state)
+  if (!(state %in% store$states)) stop('state not in store$states')
+  
+  if (variable == 'current-question'){
+    return(store$question_texts[store$states == state][[1]])
+  }
+  if (variable == 'current-answers'){
+    return(store$question_prompts[store$states == state][[1]])
+  }
+  if (variable == 'current-correct-answer'){
+    return(store$correct_answers[store$states == state][[1]])
+  }
+  if (variable == 'next-state'){
+    return(store$states[min(length(store$question_texts)+1, match(state, store$states) + 1)])
+  }
+  if (variable == 'current-response'){
+    return(store$responses[store$states == state][[1]])
+  }
+}
+
+
+#' @describeIn get_state a setter function for the state machine
+set_state <- function(store, variable, value, state = NULL){
+  if (is.null(state)) state <- get_state(store)
+  if (is.null(value)) value <- character(0)
+  
+  if (variable == 'current-state'){
+    store$state <- value
+  }
+  
+  if (variable == 'current-response'){
+    store$responses[store$states == state] <- list(value)
+  }
+  
+  return(store)
+}
+
+#' @describeIn get_state check that current-response correct
+quiz_is_current_correct <- function(store){
+  current_response <- unname(get_state(store, variable = 'current-response'))
+  current_correct_answer <- unname(get_state(store, variable = 'current-correct-answer'))
+  identical(current_response, current_correct_answer)
+}
+
+#' @describeIn get_state check that all recorded answers are correct
+quiz_is_all_correct <- function(store) {
+  tryCatch({
+    # extract responses and correct answers
+    responses <- unname(store$responses[-(length(store$question_texts) + 1)])
+    correct_answers <- unname(store$correct_answers)
+    
+    # remove names
+    responses <- purrr::map(responses, unname)
+    correct_answers <- purrr::map(correct_answers, unname)
+    
+    # check if they are the same
+    is_identical <- identical(responses, correct_answers)
+    
+    return(is_identical)
+  },
+  error = function(e) FALSE)
+}
+
+
+# helpers -----------------------------------------------------------------
+
+# adds a green checkmark to a div
+add_checkmark <- function(ns = NULL, div_id = 'submit_button'){
+  # ns <- shiny::NS(ns)
+  div_id <- ns(div_id)
+  shinyjs::runjs({
+    div_selector <- paste0('$("#', div_id, '")')
+    paste0(
+      'if (',
+      div_selector,
+      '.children().length===0){',
+      div_selector,
+      '.append("\t" + \'<span class="glyphicon glyphicon-ok" style="color:green"></span>\')}'
+    )
+  })
+}
+
+# adds a red X to a div
+add_red_x <- function(ns = NULL, div_id = 'submit_button'){
+  # ns <- shiny::NS(ns)
+  div_id <- ns(div_id)
+  shinyjs::runjs({
+    div_selector <- paste0('$("#', div_id, '")')
+    paste0(
+      'if (',
+      div_selector,
+      '.children().length===0){',
+      div_selector,
+      '.append("\t" + \'<span class="glyphicon glyphicon-remove" style="color:red"></span>\')}'
+    )
+  })
+}
+
+add_message_correct <- function(text){
+  # this relies on bootstrap css
+  shiny::div(
+    class = 'alert alert-success',
+    p(text)
+  )
+}
+
+add_message_wrong <- function(text){
+  # this relies on bootstrap css
+  shiny::div(
+    class = 'alert alert-danger',
+    p(text)
+  )
+}
+
+# https://codepen.io/zer0kool/pen/KjZWRW
+add_confetti <- function(){
+  
+  # individual confetti divs
+  html <- div(
+    class = 'confetti',
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece'),
+    div(class = 'confetti-piece')
+  )
+  
+  # css
+  html <- tagList(html, div(tags$style(
+    "
+    .confetti {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    z-index: -999;
+    }
+    .confetti-piece {
+        position: absolute;
+        width: 10px;
+        height: 30px;
+        background: #ffd300;
+        top: 0;
+        opacity: 0;
+    }
+    .confetti-piece:nth-child(1) {
+        left: 7%;
+        -webkit-transform: rotate(-40deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 182ms;
+        -webkit-animation-duration: 1116ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(2) {
+        left: 14%;
+        -webkit-transform: rotate(4deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 161ms;
+        -webkit-animation-duration: 1076ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(3) {
+        left: 21%;
+        -webkit-transform: rotate(-51deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 481ms;
+        -webkit-animation-duration: 1103ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(4) {
+        left: 28%;
+        -webkit-transform: rotate(61deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 334ms;
+        -webkit-animation-duration: 708ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(5) {
+        left: 35%;
+        -webkit-transform: rotate(-52deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 302ms;
+        -webkit-animation-duration: 776ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(6) {
+        left: 42%;
+        -webkit-transform: rotate(38deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 180ms;
+        -webkit-animation-duration: 1168ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(7) {
+        left: 49%;
+        -webkit-transform: rotate(11deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 395ms;
+        -webkit-animation-duration: 1200ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(8) {
+        left: 56%;
+        -webkit-transform: rotate(49deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 14ms;
+        -webkit-animation-duration: 887ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(9) {
+        left: 63%;
+        -webkit-transform: rotate(-72deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 149ms;
+        -webkit-animation-duration: 805ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(10) {
+        left: 70%;
+        -webkit-transform: rotate(10deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 351ms;
+        -webkit-animation-duration: 1059ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(11) {
+        left: 77%;
+        -webkit-transform: rotate(4deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 307ms;
+        -webkit-animation-duration: 1132ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(12) {
+        left: 84%;
+        -webkit-transform: rotate(42deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 464ms;
+        -webkit-animation-duration: 776ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(13) {
+        left: 91%;
+        -webkit-transform: rotate(-72deg);
+        -webkit-animation: makeItRain 1000ms infinite ease-out;
+        -webkit-animation-delay: 429ms;
+        -webkit-animation-duration: 818ms;
+        -webkit-animation-iteration-count: 3;
+    }
+    .confetti-piece:nth-child(odd) {
+        background: #7431e8;
+    }
+    .confetti-piece:nth-child(even) {
+        z-index: 1;
+    }
+    .confetti-piece:nth-child(4n) {
+        width: 5px;
+        height: 12px;
+        -webkit-animation-duration: 2000ms;
+    }
+    .confetti-piece:nth-child(3n) {
+        width: 3px;
+        height: 10px;
+        -webkit-animation-duration: 2500ms;
+        -webkit-animation-delay: 1000ms;
+    }
+    .confetti-piece:nth-child(4n-7) {
+      background: red;
+    }
+    @-webkit-keyframes makeItRain {
+        from {opacity: 0;}
+        50% {opacity: 1;}
+        to {-webkit-transform: translateY(350px);}
+    }
+    "
+  )))
+  
+  return(html)
+}
+
--- thinkCausal/modules/learning/post-treatment/post_treatment_module.R
+++ thinkCausal/modules/learning/post-treatment/post_treatment_module.R
@@ -6,6 +6,12 @@ require(tibble)
 require(ggplot2)
 
 
+# notes -------------------------------------------------------------------
+
+# ns id is set as 'concepts_post_treatment'
+# the quiz UI elements exist inside another namespace
+
+
 # objects -----------------------------------------------------------------
 
 # list to store all post-treatment objects in
@@ -14,16 +20,9 @@ store_l_post_treatment <- list()
 # set path
 store_l_post_treatment$path_to_here <- file.path('modules', 'learning', 'post-treatment')
 
-# load functions
-purrr::map(
-  list.files(file.path(store_l_post_treatment$path_to_here, 'R'), pattern = "*.R$", full.names = TRUE), 
-  source
-)
-
 # read in randomization df
-store_l_post_treatment$path_to_data <- file.path(store_l_post_treatment$path_to_here, 'data', 'plants_df.csv')
 store_l_post_treatment$plants_df <- readr::read_csv(
-  store_l_post_treatment$path_to_data,
+  file.path(store_l_post_treatment$path_to_here, 'data', 'plants_df.csv'),
   col_types = readr::cols(
     h1 = readr::col_double(),
     z = readr::col_logical(),
@@ -33,19 +32,158 @@ store_l_post_treatment$plants_df <- readr::read_csv(
 store_l_post_treatment$plants_df$z_as_text <- ifelse(store_l_post_treatment$plants_df$z, 'Treatment', 'Control')
 
 
+# set quiz structure ------------------------------------------------------
+
+# set the text for question 1
+question_1 <- tagList(
+  h3("Question 1"),
+  p("You’re tasked with determining if omega-3 fish oil supplements cause a decrease in blood pressure over a 6 month period. You have data from an experiment where participants were randomly assigned to take omega-3 fish oil supplements or a placebo supplement for 6 months. Besides the treatment variable (fish_oil) and the outcome variable (bp_6month) you have the following covariates: "),
+  tags$ul(
+    tags$li('Blood pressure measured at the start of the study (bp_baseline)'),
+    tags$li('Blood pressure measured  3 months into the study (bp_3month)'),
+    tags$li('Sex measured `at the start of the study (sex)'),
+    tags$li('Height measured at the start of the study (height)')
+  ),
+  p("Which covariates would you include in your analysis? Use the drag-drop below to move variables into the include or exclude bins.")
+)
+
+# set the UI elements for question 1
+question_prompt_1 <- sortable::bucket_list(
+  header = "Drag the variables to their respective roles",
+  group_name = NS(NS('concepts_post_treatment')('quiz'))('answers'),
+  orientation = "horizontal",
+  class = 'default-sortable sortable-wide',
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_variables'),
+    text = strong("Available"),
+    labels = c('bp_3month', 'sex', 'height'),
+    options = sortable::sortable_options(multiDrag = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_include'),
+    text = strong("Control for"),
+    labels = NULL,
+    options = sortable::sortable_options(multiDrag = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_treatment'),
+    text = strong("Treatment"),
+    labels = c('fish_oil'),
+    options = sortable::sortable_options(disabled = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_outcome'),
+    text = strong("Outcome"),
+    labels = c('bp_6month'),
+    options = sortable::sortable_options(disabled = TRUE)
+  )
+)
+
+# set the correct answers for question 1
+# answer structure must match structure provided by input$answers
+correct_answer_1 <- list(c('bp_3month'),
+                         c('sex', 'height'),
+                         c('fish_oil'),
+                         c('bp_6month'))
+
+
+# question 2 --------------------------------------------------------------
+
+# set the text for question 2
+question_2 <- tagList(
+  h3("Question 2"),
+  p("A middle school offers an optional meditation class to 8th grade students and you’re tasked with determining if the meditation class causes higher grades at the end of 8th grade. Besides the treatment variable (meditation) and the outcome variable (grades), the school provided you with several other covariates. All covariates were pulled from administrative data at the end of 8th grade. Each covariate is show below:  "),
+  # tags$ul(
+  #   tags$li('6th grade grades  measured'),
+  #   tags$li('7th grade grades'),
+  #   tags$li('number of detentions during 8th grade '),
+  #   tags$li('Race'),
+  #   tags$li('Parent income at the end of 8th grade')
+  # ),
+  # p("Lorem ipsum")
+)
+
+# set the UI elements for question 2
+question_prompt_2 <- sortable::bucket_list(
+  header = "Drag the variables to their respective roles",
+  group_name = NS(NS('concepts_post_treatment')('quiz'))('answers'),
+  orientation = "horizontal",
+  class = 'default-sortable sortable-wide',
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_variables'),
+    text = strong("Available"),
+    labels = c('6th grade grades', '7th grade grades', 'Detentions during 8th grade', 'Race'),
+    options = sortable::sortable_options(multiDrag = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_include'),
+    text = strong("Control for"),
+    labels = NULL,
+    options = sortable::sortable_options(multiDrag = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_treatment'),
+    text = strong("Treatment"),
+    labels = c('Meditation'),
+    options = sortable::sortable_options(disabled = TRUE)
+  ),
+  sortable::add_rank_list(
+    input_id = NS(NS('concepts_post_treatment')('quiz'))('answers_outcome'),
+    text = strong("Outcome"),
+    labels = c('Grades'),
+    options = sortable::sortable_options(disabled = TRUE)
+  )
+)
+
+# set the correct answers for question 2
+correct_answer_2 <- list(c('Detentions during 8th grade'),
+                         c('6th grade grades', '7th grade grades', 'Race'),
+                         c('Meditation'),
+                         c('Grades')
+)
+# use character(0) if any rank lists should be empty
+
+
+# final quiz structure ----------------------------------------------------
+
+store_l_post_treatment$question_texts <- list(question_1, question_2)
+store_l_post_treatment$question_prompts <- list(question_prompt_1, question_prompt_2)
+store_l_post_treatment$correct_answers <- list(correct_answer_1, correct_answer_2)
+store_l_post_treatment$message_correct <- "You got all of them correct"
+store_l_post_treatment$message_wrong <- "You got at least one wrong"
+
+# clean up otherwise may have conflicts with other learning modules
+rm(question_1, question_2, question_prompt_1, question_prompt_2, correct_answer_1, correct_answer_2)
+
+
 # UI ----------------------------------------------------------------------
 
 ui_post_treatment <- function(id) {
   ns <- NS(id)
   tagList(
-    wrap_with_quiz(
-      ns = ns,
-      quiz = create_quiz_post_treatment(ns = ns),
-      includeMarkdown(file.path(store_l_post_treatment$path_to_here, "markdowns", 'post_treatment_1.md')),
-      br(),
-      plotOutput(ns('posttreatment_plot'), height = 500),
-      br(),
-      includeMarkdown(file.path(store_l_post_treatment$path_to_here, "markdowns", 'post_treatment_2.md'))
+    ui <- fluidPage(
+      
+      # the quiz UI
+      ui_quiz(id = ns('quiz')),
+      
+      # UI content for the learning module
+      div(
+        class = 'learning-content', # required
+        h3('Post-treatment variables'),
+        includeMarkdown(file.path(store_l_post_treatment$path_to_here, "markdowns", 'post_treatment_1.md')),
+        br(),
+        plotOutput(ns('posttreatment_plot'), height = 500, width = 700),
+        br(),
+        includeMarkdown(file.path(store_l_post_treatment$path_to_here, "markdowns", 'post_treatment_2.md'))
+
+        # p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+        # sliderInput(inputId = ns('slider_test'),
+        #             label = 'Another UI element',
+        #             min = 0,
+        #             max = 1,
+        #             value = 0.5),
+        # textOutput(outputId = ns('text_test'))
+      )
     )
   )
 }
@@ -59,14 +197,29 @@ server_post_treatment <- function(id, plot_theme = ggplot2::theme_get) {
     id,
     function(input, output, session) {
       
+      # run the quiz
+      server_quiz(
+        id = "quiz",
+        id_parent = 'concepts_post_treatment',
+        question_texts = store_l_post_treatment$question_texts,
+        question_prompts = store_l_post_treatment$question_prompts,
+        correct_answers = store_l_post_treatment$correct_answers,
+        message_correct = store_l_post_treatment$message_correct,
+        message_wrong = store_l_post_treatment$message_wrong
+      )
+      
+      # example server to run outside of the quiz
+      # output$text_test <- renderText({
+      #   input$slider_test
+      # })
       
       # plot jitter
       output$posttreatment_plot <- renderPlot({
-        
+
         # create plot
-        p <- ggplot(data = store_l_post_treatment$plants_df, 
-                    aes(x = jitter(as.numeric(z)), y = h1, col = z_as_text)) + 
-          geom_point() + 
+        p <- ggplot(data = store_l_post_treatment$plants_df,
+                    aes(x = jitter(as.numeric(z)), y = h1, col = z_as_text)) +
+          geom_point() +
           scale_color_manual(values = c(4, 2)) +
           scale_x_continuous(labels = c('Control', 'Treatment'), breaks = c(0, 1)) +
           labs(title = 'TBD',
@@ -74,10 +227,10 @@ server_post_treatment <- function(id, plot_theme = ggplot2::theme_get) {
                x = 'Treatment status',
                y = 'Plant height',
                color = NULL)
-        
+
         # add theme
         p <- p + plot_theme()
-        
+
         return(p)
       })
     }
