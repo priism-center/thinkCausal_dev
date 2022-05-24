320250f4478688591c71b655936e3e48a3304435
Joe Marlo <joe.marlo.09@gmail.com>
2022-05-24 16:56:03 -0400

    analysis: updated footer nav to account for new pages

--- thinkCausal/UI/pages/progress_footer.R
+++ thinkCausal/UI/pages/progress_footer.R
@@ -7,48 +7,15 @@ progress_footer <- tagList(
       class = 'left-align',
       a(href="https://steinhardt.nyu.edu/priism", "New York University")
     ),
-    tags$div(
-     id = paste0('progress-footer-', module_ids$analysis$design),
-     class = 'progress-footer-tab',
-     onclick = paste0("go_to_shiny_page('", module_ids$analysis$design, "')"),
-     '1. Design'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$data),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$data, "')"),
-      '2. Data'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$eda),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$eda, "')"),
-      '3. EDA'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$model),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$model, "')"),
-      '4. Model'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$diagnostic),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$diagnostic, "')"),
-      '5. Diagnostics'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$results),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$results, "')"),
-      '6. Results'
-    ),
-    tags$div(
-      id = paste0('progress-footer-', module_ids$analysis$subgroup),
-      class = 'progress-footer-tab',
-      onclick = paste0("go_to_shiny_page('", module_ids$analysis$subgroup, "')"),
-      '7. Subgroup'
-    ),
+    purrr::map2(module_ids$analysis, seq_along(module_ids$analysis), function(tab, i){
+      display_name <- glue::glue('{i}. {stringr::str_to_sentence(names(module_ids$analysis)[i])}')
+      tags$div(
+        id = paste0('progress-footer-', tab),
+        class = 'progress-footer-tab',
+        onclick = paste0("go_to_shiny_page('", tab, "')"),
+        display_name
+      )
+    }),
     tags$div(
       class = 'right-align',
       a(onclick = 'openHelp()', style = 'cursor: pointer', "Help")
