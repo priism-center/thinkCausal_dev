3227410cbf6b979a25ab705c6f1fa16077653877
Joe Marlo <joe.marlo.09@gmail.com>
2022-05-25 11:39:49 -0400

    analysis: formating fixes for footer nav

--- thinkCausal/UI/pages/progress_footer.R
+++ thinkCausal/UI/pages/progress_footer.R
@@ -1,4 +1,4 @@
-# TODO: convert to https://codepen.io/william-goldsworthy/pen/JzVajj
+# TODO: convert to https://codepen.io/william-goldsworthy/pen/JzVajj? doesn't work great in practice
 
 progress_footer <- tagList(
   tags$div(
@@ -8,12 +8,14 @@ progress_footer <- tagList(
       a(href="https://steinhardt.nyu.edu/priism", "New York University")
     ),
     purrr::map2(module_ids$analysis, seq_along(module_ids$analysis), function(tab, i){
-      display_name <- glue::glue('{i}. {stringr::str_to_sentence(names(module_ids$analysis)[i])}')
+      # display_name <- glue::glue('{i}. {stringr::str_to_sentence(names(module_ids$analysis)[i])}')
+      display_name <- stringr::str_to_sentence(names(module_ids$analysis)[i])
+      if (i < length(module_ids$analysis)) display_name <- glue::glue('{display_name} &nbsp ➙') #→ ➝ ➔ ➔
       tags$div(
-        id = paste0('progress-footer-', tab),
+        id = glue::glue('progress-footer-{tab}'),
         class = 'progress-footer-tab',
-        onclick = paste0("go_to_shiny_page('", tab, "')"),
-        display_name
+        onclick = glue::glue('go_to_shiny_page("{tab}")'),
+        htmltools::HTML(display_name)
       )
     }),
     tags$div(
