7118d0f108b5ad2b979a1a69e705b4589e39a610
George Perrett <georgeperrett@STSFM6PQ69DY5.lan>
2022-05-27 17:30:46 -0400

    changed if else structure in fit_bart()

--- thinkCausal/R/fit_bart.R
+++ thinkCausal/R/fit_bart.R
@@ -9,19 +9,18 @@
 #' @export
 fit_bart <- function(.data, support, block, .weights, ran_eff, .estimand){
   ind <- 3 + length(.weights) + length(ran_eff)
-
+  if(is_null(.weights)){
   tryCatch({
     if(is_null(ran_eff)){
-        bartCause::bartc(
-          response = .data[, 2], 
-          treatment = .data[, 1], 
-          confounders = clean_confounders_for_bart(.data[, ind:length(.data)]), 
-          estimand = .estimand, 
-          #if(!is_null(.weights) weights = .data[[.weights]], 
-          commonSup.rule = support, 
-          keepTrees = TRUE, 
-          seed = 2
-        )
+      bartCause::bartc(
+        response = .data[, 2], 
+        treatment = .data[, 1], 
+        confounders = clean_confounders_for_bart(.data[, ind:length(.data)]), 
+        estimand = .estimand, 
+        commonSup.rule = support, 
+        keepTrees = TRUE, 
+        seed = 2
+      )
     }
     
     else{
@@ -30,7 +29,6 @@ fit_bart <- function(.data, support, block, .weights, ran_eff, .estimand){
         treatment = .data[[1]], 
         confounders = clean_confounders_for_bart(.data[, ind:length(.data)]), 
         estimand = .estimand, 
-        weights = wgt, 
         group.by = ran_eff, 
         use.ranef = TRUE,  
         commonSup.rule = support, 
@@ -38,7 +36,40 @@ fit_bart <- function(.data, support, block, .weights, ran_eff, .estimand){
         seed = 2
       )
     }
+  },
+  error = function(e) NULL
+  )
+  }else{
+    tryCatch({
+      if(is_null(ran_eff)){
+        bartCause::bartc(
+          response = .data[, 2], 
+          treatment = .data[, 1], 
+          confounders = clean_confounders_for_bart(.data[, ind:length(.data)]), 
+          estimand = .estimand, 
+          weights = .data[[.weights]], 
+          commonSup.rule = support, 
+          keepTrees = TRUE, 
+          seed = 2
+        )
+      }
+      
+      else{
+        bartCause::bartc(
+          response = .data[[2]], 
+          treatment = .data[[1]], 
+          confounders = clean_confounders_for_bart(.data[, ind:length(.data)]), 
+          estimand = .estimand, 
+          weights = .data[[.weights]], 
+          group.by = ran_eff, 
+          use.ranef = TRUE,  
+          commonSup.rule = support, 
+          keepTrees = TRUE,
+          seed = 2
+        )
+      }
     },
     error = function(e) NULL
-  )
+    )
+  }
 }
--- thinkCausal/modules/analysis/7_model/model_server.R
+++ thinkCausal/modules/analysis/7_model/model_server.R
@@ -11,7 +11,7 @@ server_model <- function(store, id, global_session){
       })
       
       # update variables on the model page once the save button on the verify data page is clicked
-      observeEvent(store$analysis$data$verify$analysis_data_save, {
+      observeEvent(store$analysis$data$verify$analysis_verify_data_save, {
         
         # create list of moderator combinations on model page
         cols_categorical <- store$column_types$categorical
